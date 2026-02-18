6b03bd2788128872ccc53fb26c6ffa1dba3589ea
FirstName LastName <re.lopezm1@uniandes.edu.co>
2026-02-18 08:56:24 -0500

    act

--- src/test/java/co/edu/uniandes/dse/TallerPersistencia/services/PeliculaServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/TallerPersistencia/services/PeliculaServiceTest.java
@@ -1,4 +1,4 @@
-Dpackage co.edu.uniandes.dse.TallerPersistencia.services;
+package co.edu.uniandes.dse.TallerPersistencia.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
